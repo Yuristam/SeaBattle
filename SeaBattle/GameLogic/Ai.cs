ed839555716e054379612d5f61d2f41e285c5292
Yuristam <yuristamasanakunov@gmail.com>
2023-03-09 19:49:14 +0600

    updated whole project

--- SeaBattle/GameLogic/Ai.cs
+++ SeaBattle/GameLogic/Ai.cs
@@ -12,10 +12,16 @@ namespace SeaBattle.GameLogic
                 int index = random.Next(0, 10);
                 int index2 = random2.Next(0, 10);
 
-                if (playerCells[index, index2] == 'X')
+                if (playerCells[index, index2] == 'X' || playerCells[index, index2] == 'O')
                 {
                     random.Next(0, 9);
                     random2.Next(0, 9);
+                }
+                else if(playerCells[index, index2] == '#')
+                {
+                    playerCells[index, index2] = 'O';
+                    //comp shot your ship
+                    break;
                 }
                 else
                 {
--- SeaBattle/Menu/Input.cs
+++ SeaBattle/Menu/Input.cs
@@ -1,8 +1,64 @@
-﻿using static SeaBattle.Graphics.Board;
+﻿using SeaBattle.GameLogic;
+using static SeaBattle.Graphics.Board;
 namespace SeaBattle.Menu
 {
     public static class Input
     {
+        public static void UserShips()
+        {
+                Console.Clear();
+                for (int k = 1; k < 11; k++)
+                {
+                    Console.Write(k + " ");
+                }
+                Console.WriteLine();
+
+                for (int k = 0; k < playerCells.GetLength(0); k++)
+                {
+                    for (int j = 0; j < playerCells.GetLength(1); j++)
+                    {
+                        Console.Write(playerCells[k, j] + " ");
+                    }
+                    Console.WriteLine(k + 1);
+                }
+                //===========================================================================
+
+            while (true)
+            {
+                Console.Write("Choose your ships.\n");
+                Console.Write("Choose the row\n>");
+                string userInput = Console.ReadLine().ToLower().Trim();
+                Console.Write("Choose the column\n>");
+                string userInput2 = Console.ReadLine().ToLower().Trim();
+                //==========================================================================
+                if (int.TryParse(userInput, out int index) && int.TryParse(userInput2, out int index2))
+                {
+                    if (index <= 0 || index > 10 || index2 <= 0 || index2 > 10)
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                        Console.Write("There is no such command, please try another command.\n>");
+                        Console.ResetColor();
+                    }
+                    else if (playerCells[index - 1, index2 - 1] == '#')
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                        Console.WriteLine("You've already chosen this cells, please choose another cell.");
+                        Console.ResetColor();
+                    }
+                    else
+                    {
+                        playerCells[index - 1, index2 - 1] = '#';
+                        break;
+                    }
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.Write("There is no such command, please try another command.\n>");
+                    Console.ResetColor();
+                }
+            }
+        }
         public static void UserInputs()
         {
             // TODO  заменить первый индекс на буквы и после сделать 1 инпут
@@ -21,12 +77,31 @@ namespace SeaBattle.Menu
                         Console.Write("There is no such command, please try another command.\n>");
                         Console.ResetColor();
                     }
-                    else if (enemyCells[index - 1, index2 - 1] == 'X')
+                    else if (enemyCells[index - 1, index2 - 1] == 'X'|| enemyCells[index - 1, index2 - 1] == 'O')
                     {
                         Console.ForegroundColor = ConsoleColor.DarkRed;
                         Console.WriteLine("You've already attacked here, please choose another cell.");
                         Console.ResetColor();
                     }
+                    //=============================================================================
+                    // todo solve problem here
+                    else if (index - 1 == Win.shipX1 && index2 - 1 == Win.shipY1)
+                    {
+                        enemyCells[Win.shipX1, Win.shipY1] = 'O';
+                        break;
+                    }
+                    else if (index - 1 == Win.shipX2 && index2 - 1 == Win.shipY2)
+                    {
+                        enemyCells[Win.shipX2, Win.shipY2] = 'O';
+                        break;
+                    }
+                    else if (index - 1 == Win.shipX3 && index2 - 1 == Win.shipY3)
+                    {
+                        enemyCells[Win.shipX3, Win.shipY3] = 'O';
+                        break;
+                    }
+                    //todo loop here
+                    //========================================================================================
                     else
                     {
                         enemyCells[index - 1, index2 - 1] = 'X';
